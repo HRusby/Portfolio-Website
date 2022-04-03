5c11b49b6f4133fc2e43a0318bc41545fb0179f4
HRusby <harrygeorger@hotmail.com>
2022-04-03 18:46:06 +0100

    Styling SharedHeader

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<style>
+  html {
+    background-color: #0D1117; /* Github Dark Bg */
+    color: white;
+    height: 4vh;
+  }
+</style>
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
   import SharedHeader from "./components/SharedHeader.vue"
@@ -5,9 +12,7 @@
 </script>
 
 <template>
-  <header>
-    <SharedHeader />
-  </header>
+  <SharedHeader />
   <RouterView />
 </template>
 
--- src/components/SharedHeader.vue
+++ src/components/SharedHeader.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts"></script>
-
+<style>
+  .navBarRouterLink:hover {
+    color: white;
+    font-weight: 600;
+  }
+</style>
 <template>
-  <header>
-    <a href="/">
-      <h3>Harry<span style="color:red">.</span>Rusby</h3>
+  <header class="sticky top-0 w-screen justify-between px-4 py-4 bg-zinc-500 inline-block">
+    <a href="/" class="text-2xl font-medium float-left">
+      <h3 style="display:inline">Harry<span style="color:red">.</span>Rusby</h3>
     </a>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/blog">Blog</RouterLink>
+    <nav class="space-x-8 inline-block float-right">
+      <RouterLink class="navBarRouterLink" to="/">Home</RouterLink>
+      <RouterLink class="navBarRouterLink" to="/about">About</RouterLink>
+      <RouterLink class="navBarRouterLink" to="/blog">Blog</RouterLink>
     </nav>
   </header>
 </template>
