b10fff04daff094099f8418bd071b66335fcda6c
HRusby <harrygeorger@hotmail.com>
2022-04-10 13:03:20 +0100

    Adding BlogCard and Demo Cards for BlogView

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,16 @@
+<style scoped>
+  
+</style>
+<script setup lang="ts">
+  defineProps({
+    title: String,
+    blurb: String
+  })
+</script>
+
+<template>
+  <div class="bg-slate-500 min-h-xl shadow-md shadow-gray-600">
+    <h1 class="font-extrabold text-xl">{{title}}</h1>
+    <p>{{blurb}}</p>
+  </div>
+</template>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+  import BlogCard from '../components/BlogCard.vue'
 </script>
 
 <template>
-  <p class="text-3xl font-bold underline hover:bg-sky-700">This is Some Blog</p>
+  <div class="grid grid-cols-5 gap-4 w-4/5 mx-auto">
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+    <BlogCard title="This is a Blog Title" blurb="And here is the summary of the Blog Post"></BlogCard>
+  </div>
 </template>
