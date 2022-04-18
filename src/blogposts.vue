3ad3ba9429650485a6ccfb2c04cc85ebabda937b
HRusby <harrygeorger@hotmail.com>
2022-04-18 12:10:28 +0100

    Moving blogposts.json to blogposts.vue to allow for Typing

---
+++ src/blogposts.vue
@@ -0,0 +1,14 @@
+let posts: {id:number, name: String, filepath:String}[] = [ 
+    {
+      "id": 1,
+      "name": "test",
+    "filepath": "MarkdownFiles/test.md"
+    },
+    {
+      "id": 2,
+      "name": "test2",
+      "filepath": "MarkdownFiles/test2.md"
+    }
+  ]
+
+export default posts;
