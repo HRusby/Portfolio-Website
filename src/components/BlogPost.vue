c72af9eaf938441fd49cc98ed076b34bafbb8ccc
HRusby <harrygeorger@hotmail.com>
2022-04-18 12:11:13 +0100

    Updating BlogPost/View

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,30 +1,29 @@
-<style scoped>
-</style>
+<style scoped></style>
 
 <script lang="ts" setup>
-  import {marked} from 'marked';
-  import DOMPurify from 'dompurify';
-  import { computed, onBeforeMount, onMounted, reactive } from 'vue';
-  defineProps({
-    markdown: String,
-  })
+import { marked } from "marked";
+import DOMPurify from "dompurify";
+import { computed, onBeforeMount, onMounted, reactive } from "vue";
+defineProps({
+  markdown: String,
+});
 
-  const state = reactive({
-    md: null,
-  })
-  
-  async function markdownToHtml(){
-    state.md= await fetch('./MarkdownFiles/test.md')
-          .then(response => response.text())
-          .then(resp => marked.parse(resp))
-  } 
+const state: { md: string | null } = reactive({
+  md: null,
+});
 
-  onBeforeMount(() => { 
-    markdownToHtml()
-  })
+async function markdownToHtml() {
+  state.md = await fetch("./MarkdownFiles/test.md")
+    .then((response) => response.text())
+    .then((resp) => DOMPurify.sanitize(marked.parse(resp)))
+    .then((resp) => (resp === null ? "" : resp));
+}
+
+onBeforeMount(() => {
+  markdownToHtml();
+});
 </script>
 
 <template>
-  <div v-html="state.md"/>
-    
+  <div v-html="state.md" />
 </template>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-  import BlogCard from '../components/BlogCard.vue'
+  import BlogCard from "../components/BlogCard.vue";
+  import {default as posts} from '../blogposts.vue';
 
-  const BlogPosts = import.meta.glob('../../public/MarkdownFiles/*.md')
-  let postNames:String[] = []
+  console.log(posts[0])
+  const BlogPosts = import.meta.glob("../../public/MarkdownFiles/*.md");
+  let postNames: string[] = [];
   for (const path in BlogPosts) {
-    postNames.push(BlogPosts[path].name)
+    let name = BlogPosts[path].name;
+    let postName = name.substring(name.indexOf("MarkdownFiles/") + 14);
+    postNames.push(postName);
   }
 </script>
-
 
 <template>
   <div class="grid grid-cols-5 gap-4 w-4/5 mx-auto">
